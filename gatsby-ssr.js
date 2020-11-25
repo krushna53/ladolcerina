dfceb166b1370b4a019ad1d1e95707c46b1f674c
Eric Manongdo <superscript@Erics-MBP.home>
2020-11-25 12:25:20 -1000

    Added mailchimp integration ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,13 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require("react")
 
-// You can delete this file if you're not using it
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      id="mcjs"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/20bb22ff7fdb4f70b68522c88/d087b1c8bd7a893fd27b9c281.js");`,
+      }}
+    />, //Injecting mailchimp popup in Head
+  ])
+}
